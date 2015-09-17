ea40cf1a3d3e6f231ba7182fed5a32ba67864c26
kangwl <kangwl2009@163.com>
2015-09-17 17:32:07 +0800

    kang

---
+++ XK.SearchEngine.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XK.SearchEngine.Test {
+    class Program {
+        static void Main(string[] args) {
+            // XK.SearchEngine.IndexManage.CreateIndex("中秋节快乐", "这是一个节日，中秋节哦");
+            // Console.WriteLine("ok");
+
+            string[] fields = {"Title", "Content" };
+            int total;
+            List<News> news = XK.SearchEngine.IndexManage.Search<News>("中秋", fields,10,0, out total);
+            Console.WriteLine(news[0].Content);
+            Console.Read();
+        }
+
+        class News {
+            public string Title { get; set; }
+            public string Content { get; set; } 
+        }
+    }
+}
