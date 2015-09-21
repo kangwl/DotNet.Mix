c8f708169cabac73dca9ecdf220dee9702c816e2
kangwl <kangwl2009@163.com>
2015-09-21 11:06:17 +0800

    delete pangu search

--- XK.PanGuSearch/SearchResult_Model.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XK.PanGuSearch {
-    public class SearchResult_Model<TModel> where TModel :class {
- 
-        public TModel Data { get; set; }
-        public int Total { get; set; }
-    }
-}
